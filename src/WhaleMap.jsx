77f81d208722947bbb4cdd9e33b3305ab4b763aa
Gordon06060 <gordonz0913@gmail.com>
2025-01-11 16:00:40 -0500

    feat: added map

--- src/WhaleMap.jsx
+++ src/WhaleMap.jsx
@@ -12,10 +12,22 @@ import React from "react";
 
 export default function WhaleMap() {
   const position = { lat: 53.45, lng: 10 };
+
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_API_KEY}>
       <div style={{ height: "100vh", width: "100%" }}>
-        <Map zoom={9} center={position}></Map>
+        <Map
+          defaultZoom={13}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged={(ev) =>
+            console.log(
+              "camera changed:",
+              ev.detail.center,
+              "zoom:",
+              ev.detail.zoom
+            )
+          }
+        />
       </div>
     </APIProvider>
   );
