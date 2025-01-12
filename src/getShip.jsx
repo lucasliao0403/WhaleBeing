d31b853cb45a7d14c6b5b8af3fc9fc099b905f46
raphael-64 <raphlee2006@gmail.com>
2025-01-11 19:10:08 -0500

    fix: "submit" changed to "search"

--- src/getShip.jsx
+++ src/getShip.jsx
@@ -103,7 +103,7 @@ export default function TextInput(props) {
           className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
           onClick={handleSubmit}
         >
-          Submit
+          Search
         </button>
       </div>
 
